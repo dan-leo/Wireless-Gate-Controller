8944b3b40e8f8a9012b32bd38677efcc84981ae8
Daniel Robinson <d7robin@gmail.com>
2016-02-16 01:06:20 +0200

    printing chars to all 16 5x7 dot matrices

--- src/r_main.c
+++ src/r_main.c
@@ -1,39 +1,39 @@
 /***********************************************************************************************************************
-* DISCLAIMER
-* This software is supplied by Renesas Electronics Corporation and is only 
-* intended for use with Renesas products. No other uses are authorized. This 
-* software is owned by Renesas Electronics Corporation and is protected under 
-* all applicable laws, including copyright laws.
-* THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING 
-* THIS SOFTWARE, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING BUT NOT 
-* LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
-* AND NON-INFRINGEMENT.  ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.
-* TO THE MAXIMUM EXTENT PERMITTED NOT PROHIBITED BY LAW, NEITHER RENESAS 
-* ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES SHALL BE LIABLE 
-* FOR ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR 
-* ANY REASON RELATED TO THIS SOFTWARE, EVEN IF RENESAS OR ITS AFFILIATES HAVE 
-* BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-* Renesas reserves the right, without notice, to make changes to this software 
-* and to discontinue the availability of this software.  By using this software, 
-* you agree to the additional terms and conditions found by accessing the 
-* following link:
-* http://www.renesas.com/disclaimer
-*
-* Copyright (C) 2011, 2013 Renesas Electronics Corporation. All rights reserved.
-***********************************************************************************************************************/
+ * DISCLAIMER
+ * This software is supplied by Renesas Electronics Corporation and is only
+ * intended for use with Renesas products. No other uses are authorized. This
+ * software is owned by Renesas Electronics Corporation and is protected under
+ * all applicable laws, including copyright laws.
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING
+ * THIS SOFTWARE, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING BUT NOT
+ * LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NON-INFRINGEMENT.  ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.
+ * TO THE MAXIMUM EXTENT PERMITTED NOT PROHIBITED BY LAW, NEITHER RENESAS
+ * ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES SHALL BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR
+ * ANY REASON RELATED TO THIS SOFTWARE, EVEN IF RENESAS OR ITS AFFILIATES HAVE
+ * BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ * Renesas reserves the right, without notice, to make changes to this software
+ * and to discontinue the availability of this software.  By using this software,
+ * you agree to the additional terms and conditions found by accessing the
+ * following link:
+ * http://www.renesas.com/disclaimer
+ *
+ * Copyright (C) 2011, 2013 Renesas Electronics Corporation. All rights reserved.
+ ***********************************************************************************************************************/
 
 /***********************************************************************************************************************
-* File Name    : r_main.c
-* Version      : CodeGenerator for RL78/G14 V2.02.00.01 [25 Dec 2013]
-* Device(s)    : R5F104LE
-* Tool-Chain   : GCCRL78
-* Description  : This file implements main function.
-* Creation Date: 2016-02-14
-***********************************************************************************************************************/
+ * File Name    : r_main.c
+ * Version      : CodeGenerator for RL78/G14 V2.02.00.01 [25 Dec 2013]
+ * Device(s)    : R5F104LE
+ * Tool-Chain   : GCCRL78
+ * Description  : This file implements main function.
+ * Creation Date: 2016-02-14
+ ***********************************************************************************************************************/
 
 /***********************************************************************************************************************
 Includes
-***********************************************************************************************************************/
+ ***********************************************************************************************************************/
 #include "r_cg_macrodriver.h"
 #include "r_cg_cgc.h"
 #include "r_cg_port.h"
@@ -63,82 +63,95 @@ void toggle_led();
 
 /***********************************************************************************************************************
 Global variables and functions
-***********************************************************************************************************************/
+ ***********************************************************************************************************************/
 /* Start user code for global. Do not edit comment generated here */
 void delay(uint16_t delay);
 /* End user code. Do not edit comment generated here */
 void R_MAIN_UserInit(void);
 
 /***********************************************************************************************************************
-* Function Name: main
-* Description  : This function implements main function.
-* Arguments    : None
-* Return Value : None
-***********************************************************************************************************************/
+ * Function Name: main
+ * Description  : This function implements main function.
+ * Arguments    : None
+ * Return Value : None
+ ***********************************************************************************************************************/
 void main(void)
 {
-    R_MAIN_UserInit();
-    /* Start user code. Do not edit comment generated here */
-    uart1TxBuf[0] = 'a';
-    while (1U)
-    {
-    	if (timer0_interrupt)
-    	{
-    		timer0_interrupt = 0;
-//    		toggle_led();
-//    		P6 ^= 0xFF;
-//    		P5 ^= 0xFF;
-//    		P4 ^= 0xFF;
-//    		P3 ^= 0xFF;
-//    		P2 ^= 0xFF;
-    		uart1Status = R_UART1_Send(&uart1TxBuf[0], 1);
-    	}
+	R_MAIN_UserInit();
+	/* Start user code. Do not edit comment generated here */
+	uart1TxBuf[0] = 'a';
+	while (1U)
+	{
+		if (timer0_interrupt)
+		{
+			timer0_interrupt = 0;
+			//    		toggle_led();
+			//    		P6 ^= 0xFF;
+			//    		P5 ^= 0xFF;
+			//    		P4 ^= 0xFF;
+			//    		P3 ^= 0xFF;
+			//    		P2 ^= 0xFF;
+			uart1Status = R_UART1_Send(&uart1TxBuf[0], 1);
+		}
 
-//        delay(1000);
-//        P6 ^= 0x1;
-//        delay(1000);
-    }
-    /* End user code. Do not edit comment generated here */
+		//        delay(1000);
+		//        P6 ^= 0x1;
+		//        delay(1000);
+	}
+	/* End user code. Do not edit comment generated here */
 }
 
 
 /***********************************************************************************************************************
-* Function Name: R_MAIN_UserInit
-* Description  : This function adds user code before implementing main function.
-* Arguments    : None
-* Return Value : None
-***********************************************************************************************************************/
+ * Function Name: R_MAIN_UserInit
+ * Description  : This function adds user code before implementing main function.
+ * Arguments    : None
+ * Return Value : None
+ ***********************************************************************************************************************/
 void R_MAIN_UserInit(void)
 {
-    /* Start user code. Do not edit comment generated here */
-    EI();
-    R_TAU0_Create();
-    R_TAU0_Channel0_Start();
+	/* Start user code. Do not edit comment generated here */
+	EI();
+	R_TAU0_Create();
+	R_TAU0_Channel0_Start();
 
-    // R_UART1_Create();
-    R_UART1_Start();
+	// R_UART1_Create();
+	R_UART1_Start();
 
-    //PM7 = 0x7F;
-    // P7 = 0x7F;
+	//PM7 = 0x7F;
+	// P7 = 0x7F;
 
-//    PM6 = 0x00;
-//	PM5 = 0x00;
-//	PM4 = 0x00;
-//	PM3 = 0x00;
-//	PM2 = 0x00;
-//
-    initLcd();
+	//    PM6 = 0x00;
+	//	PM5 = 0x00;
+	//	PM4 = 0x00;
+	//	PM3 = 0x00;
+	//	PM2 = 0x00;
+	//
+	initLcd();
+	// writeByteLcd(0U, 0x1);
+	// writeByteLcd(0U, 0x2);
+	int i;
+	char lcd_message[37] = {'H','a','l','l','e','l','u','j','a','h','!','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	for (i = 0; i < 37; i++)
+	{
+		writeByteLcd(1U, lcd_message[i]);
+		int j = 0;
+		for ( ; j < 100; j++);
+		// writeByteLcd(0U, LCD_CURSOR_RIGHT);
+		// for (j = 0 ; j < 100; j++);
+		// writeByteLcd(1U, lcd_message[i]);
+		if (i == 7){
+			int k;
+			for (k = 0; k < 32; k++)
+			{
+				writeByteLcd(0U, LCD_CURSOR_RIGHT);
+				for (j = 0 ; j < 100; j++);
+			}
+		}
+	}
 
-    int i;
-    char lcd_message[11] = {'H','a','l','l','e','l','u','j','a','h','!'};
-    for (i = 0; i < 11; i++)
-    {
-    	writeByteLcd(1U, lcd_message[i]);
-    	writeByteLcd(1U, lcd_message[i]);
-    }
-
-    uart1Status = R_UART1_Receive(&uart1RxBuf[0],1);	// Prime UART1 Rx
-    /* End user code. Do not edit comment generated here */
+	uart1Status = R_UART1_Receive(&uart1RxBuf[0],1);	// Prime UART1 Rx
+	/* End user code. Do not edit comment generated here */
 }
 
 /* Start user code for adding. Do not edit comment generated here */
